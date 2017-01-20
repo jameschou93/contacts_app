bf9060312133d1747bfa2e5734f7a2e8fc58e5e6
James Chou <jameschou1993@gmail.com>
2017-01-19 20:49:26 -0500

    added form to add contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,5 +6,13 @@ def display
   render 'contacts.html.erb'
 end
 
+def form
+  
+end
+
+def result
+  @contact = Contact.create(first_name: params[:input_first_name], last_name: params[:input_last_name], email: params[:input_email], phone_number: params[:input_phone_number])
+
+end
 
 end
