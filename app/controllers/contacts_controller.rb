164c0659a9bc78734bce59f1e8acf6e824511e6c
James Chou <jameschou1993@gmail.com>
2017-01-23 19:53:42 -0500

    RESTFUL

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,37 @@
-class ContactsController < ApplicationController
+class ContactsController< ApplicationController
 
-def display
-  @contacts = Contact.all
-  p @contacts
-  render 'contacts.html.erb'
-end
-
-def form
+def index
+  @contact = Contact.all
   
 end
 
-def result
-  @contact = Contact.create(first_name: params[:input_first_name], last_name: params[:input_last_name], email: params[:input_email], phone_number: params[:input_phone_number])
-
+def new
+  
 end
 
+def create
+  @contact = Contact.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
+
+end
+def edit
+  @contact = Contact.find_by(id: params[:id])
+  render 'edit.html.erb'
+end
+
+def update
+  @contact = Contact.find_by(id: params[:id])
+  @contact.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
+  render 'update.html.erb'
+  @contact.save
+end
+
+def show
+  @contact = Contact.find_by(id: params[:id])
+end
+
+def destroy
+  @contact = Contact.find_by(id: params[:id])
+  @contact.delete
+  render 'destroy.html.erb'
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-get 'contacts', to: 'contacts#display'
-get 'form', to: 'contacts#form'
-post 'result', to: 'contacts#result'
+root to: 'contacts#index'
+get '/contacts', to: 'contacts#index'
+get '/contacts/new', to: 'contacts#new'
+post '/contacts', to: 'contacts#create'
+get '/contacts/:id/edit' , to: 'contacts#edit'
+patch '/contacts/:id', to: 'contacts#update'
+get '/contacts/:id', to: 'contacts#show'
+delete '/contacts/:id', to: 'contacts#destroy'
 
 end
